3796b23363ec559306a142f175a173ba8c95c6bb
jhondy manrique <jhondy.manrique@misena.edu.co>
2025-08-30 08:47:54 -0500

    se añadio funcion al orderDAO para filtrar por fechas y mostrar las ordenes en la tabla orders.jsp

--- src/main/java/com/djabu/controller/OrderServletController.java
+++ src/main/java/com/djabu/controller/OrderServletController.java
@@ -9,13 +9,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/orders")
 public class OrderServletController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<OrderModel> orders = new OrderDAO().getAllOrders();
+
+        String startDateString = request.getParameter("startDate");
+        String endDateString = request.getParameter("endDate");
+        OrderDAO orderDAO = new OrderDAO();
+        List<OrderModel> orders;
+
+        if (startDateString != null && !startDateString.isEmpty() && endDateString != null && !endDateString.isEmpty()) {
+            // Si hay fechas, las convertimos y llamamos al método de filtrado
+            LocalDate startDate = LocalDate.parse(startDateString);
+            LocalDate endDate = LocalDate.parse(endDateString);
+            orders = orderDAO.getOrdersByDate(startDate,endDate);
+        } else {
+            // Si no hay fechas, obtenemos todas las ventas
+            orders = orderDAO.getAllOrders();
+        }
+
         request.setAttribute("orders", orders);
         request.getRequestDispatcher("/WEB-INF/view/orders.jsp").forward(request, response);
     }
