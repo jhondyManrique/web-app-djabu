915951c6b43c0d8fe81d2ff463d8e0628f0d40b7
jhondy manrique <jhondy.manrique@misena.edu.co>
2025-11-01 11:30:33 -0500

    se crea la clase de configuracion de la pool connection

---
+++ src/main/java/com/djabu/config/DatabaseConfig.java
@@ -0,0 +1,48 @@
+package com.djabu.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DatabaseConfig {
+
+    private  static final HikariDataSource dataSource;
+
+    private static final String JDBC_URL = System.getenv("POSTGRESQL_JDBC_URL");
+    private static final String JDBC_USER = System.getenv("POSTGRESQL_JDBC_USER");
+    private static final String JDBC_PASSWORD = System.getenv("POSTGRESQL_JDBC_PASSWORD");
+    private static final String JDBC_DRIVER = System.getenv("org.postgresql.Driver");
+
+    static{
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("JDBC class not found", e);
+        }
+
+        HikariConfig config = new HikariConfig();
+        config.setUsername(JDBC_USER);
+        config.setJdbcUrl(JDBC_URL);
+        config.setPassword(JDBC_PASSWORD);
+        config.setDriverClassName(JDBC_DRIVER);
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setIdleTimeout(30000);
+        config.setMaxLifetime(60000);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static HikariDataSource getDataSource() {
+        return dataSource;
+    }
+
+    public static void shutDown() {
+        if(dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+
+
+
+
+}
